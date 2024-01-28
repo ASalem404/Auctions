b449860b015a6979ef0c54b6d72ebc2249a3d39d
Ahmed Salem <as2720094@gmail.com>
2024-01-28 16:21:15 +0200

    :sparkles: add the body to the dynamoDB table

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,9 +1,28 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
 
+  const auction = {
+    id: uuid(),
+    title,
+    status: "OPEN",
+    createdAt: new Date().toISOString(),
+  };
+
+  await dynamoDB
+    .put({
+      TableName: "AuctionsTable",
+      Item: auction,
+    })
+    .promise();
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ title }),
+    statusCode: 201,
+    body: JSON.stringify(auction),
   };
 }
 
