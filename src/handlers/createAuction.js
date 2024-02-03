c52a2758ade141a1115e3250d9419b463381753a
Ahmed Salem <as2720094@gmail.com>
2024-02-03 22:04:24 +0200

    :sparkles: using middy as a middleware

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,4 +1,9 @@
 import { v4 as uuid } from "uuid";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
 import AWS from "aws-sdk";
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
@@ -12,13 +17,17 @@ async function createAuction(event, context) {
     status: "OPEN",
     createdAt: new Date().toISOString(),
   };
-
-  await dynamoDB
-    .put({
-      TableName: process.env.AUCTIONS_TABLE_NAME,
-      Item: auction,
-    })
-    .promise();
+  try {
+    await dynamoDB
+      .scan({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Item: auction,
+      })
+      .promise();
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
@@ -26,4 +35,7 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
