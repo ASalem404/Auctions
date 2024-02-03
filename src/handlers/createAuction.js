d8f0f278e0cf5f7577c30458ef81ed2b20408ebd
Ahmed Salem <as2720094@gmail.com>
2024-02-03 22:46:26 +0200

    :wrench: use the common middleware instead of repeating it

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,8 +1,5 @@
 import { v4 as uuid } from "uuid";
-import middy from "@middy/core";
-import httpJsonBodyParser from "@middy/http-json-body-parser";
-import httpEventNormalizer from "@middy/http-event-normalizer";
-import httpErrorHandler from "@middy/http-error-handler";
+import commonMiddleware from "../../lib/commonMiddleware";
 import createError from "http-errors";
 import AWS from "aws-sdk";
 
@@ -35,7 +32,4 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = middy(createAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(createAuction);
